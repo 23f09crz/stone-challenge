8fd655b7959405aea9528378f82776b52bdf141e
josevalencar <jose.silva@sou.inteli.edu.br>
2024-08-24 21:07:56 -0300

    FEAT. landing page cores, fontes e textos (falta imagens)

---
+++ src/landing-page/src/components/Description/index.jsx
@@ -0,0 +1,30 @@
+import styles from './style.module.scss';
+import { useInView, motion } from 'framer-motion';
+import { useRef } from 'react';
+import { slideUp, opacity } from './animation';
+import Rounded from '../../common/RoundedButton';
+export default function index() {
+
+    const phrase = "Nosso objetivo é claro: ajudar quem empreende no Brasil. Acreditamos que a tecnologia é a chave para o sucesso de qualquer negócio. Por isso, criamos um assistente no WhatsApp com Inteligência Artificial para microempreendedores. Desenvolvido para Stone, o Toni oferece uma solução digital, humanizada e extremamente intuitiva.";
+    const description = useRef(null);
+    const isInView = useInView(description)
+    return (
+        <div ref={description} className={styles.description}>
+            <div className={styles.body}>
+                <p>
+                {
+                    phrase.split(" ").map( (word, index) => {
+                        return <span key={index} className={styles.mask}><motion.span variants={slideUp} custom={index} animate={isInView ? "open" : "closed"} key={index}>{word}</motion.span></span>
+                    })
+                }
+                </p>
+                <motion.p variants={opacity} animate={isInView ? "open" : "closed"}>Agora, você pode gerenciar suas vendas, finanças e ainda melhorar o atendimento ao cliente diretamente pelo aplicativo mais utilizado no Brasil: o WhatsApp, de forma fácil e rápida.</motion.p>
+                <div data-scroll data-scroll-speed={0.1}>
+                    <Rounded className={styles.button}>
+                        <p>Acessar o Toni</p>
+                    </Rounded>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/landing-page/src/data.js
@@ -0,0 +1,37 @@
+export const projects = [
+  {
+    title: "Gestão Financeira Simplificada",
+    description: "Com Toni, você tem total controle sobre as finanças do seu negócio. Acompanhe suas vendas diárias, gere relatórios financeiros instantâneos e receba lembretes sobre suas contas a pagar diretamente no WhatsApp. Tudo isso sem complicações e em tempo real.",
+    src: "rock.jpg",
+    link: "https://www.ignant.com/2023/03/25/ad2186-matthias-leidingers-photographic-exploration-of-awe-and-wonder/",
+    color: "#87CEEB"
+  },
+  {
+    title: "Análises e Insights Personalizados",
+    description: "Toni vai além dos números; ele transforma dados em insights acionáveis. Descubra quais são seus clientes mais valiosos, os produtos mais vendidos e receba sugestões personalizadas para aumentar suas vendas e melhorar a gestão do seu estoque.",
+    src: "tree.jpg",
+    link: "https://www.ignant.com/2022/09/30/clement-chapillon-questions-geographical-and-mental-isolation-with-les-rochers-fauves/",
+    color: "#4169E1"
+  },
+  {
+    title: "Assistência Inteligente via Áudio",
+    description: "O seu tempo é valioso e Toni entende isso. Muitos microempreendedores não sabem ler ou escrever. Com a funcionalidade de áudio, você pode enviar comandos e perguntas sem precisar digitar. Simplesmente fale e deixe que Toni faça o resto.",
+    src: "water.jpg",
+    link: "https://www.ignant.com/2023/10/28/capturing-balis-many-faces-zissou-documents-the-sacred-and-the-mundane-of-a-fragile-island/",
+    color: "#88A28D"
+  },
+  {
+    title: "Relatórios e Lembretes Automatizados",
+    description: "Toni mantém você informado sobre todos os aspectos do seu negócio. Receba relatórios diários automáticos sobre o desempenho das vendas e lembretes sobre compromissos financeiros, tudo para garantir que nada passe despercebido.",
+    src: "house.jpg",
+    link: "https://www.ignant.com/2019/03/13/a-photographic-series-depicting-the-uncertain-future-of-denmarks-treasured-coastlines/",
+    color: "#44A0A0"
+  },
+  {
+    title: "Integração com Open Finance",
+    description: "Preparando-se para o futuro, Toni trará a integração com Open Finance, permitindo que você conecte suas contas bancárias e centralize a gestão financeira do seu negócio. Isso significa mais controle e menos preocupações na hora de organizar suas finanças.",
+    src: "cactus.jpg",
+    link: "https://www.ignant.com/2023/04/12/mark-rammers-all-over-again-is-a-study-of-regret-and-the-willingness-to-move-forward/",
+    color: "#2ECC71"
+  }
+]

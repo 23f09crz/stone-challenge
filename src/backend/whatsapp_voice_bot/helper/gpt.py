1e0e314cb8147ed5503b772fdf90d16070206a2a
23f09crz <pedroaccruz@gmail.com>
2024-08-25 11:42:04 -0300

    FEAT AI

--- src/backend/whatsapp_voice_bot/helper/gpt.py
+++ src/backend/whatsapp_voice_bot/helper/gpt.py
@@ -1,20 +1,18 @@
 import os
-from openai import OpenAI
-from dotenv import load_dotenv
+import uuid
 
-# Load environment variables from a .env file
-load_dotenv()
+import openai
+import requests
+import soundfile as sf
 
-# Retrieve your OpenAI API key from the environment variables
-openai_key = os.getenv('OPENAI_API_KEY')
+from config import config
 
-# Initialize the OpenAI client
-client = OpenAI(api_key=openai_key)
+openai.api_key = config.OPENAI_API_KEY
 
-# Initial chat log with a system message
 chat_log = [
     {"role": "system", "content": "Você é um assistente financeiro chamado Toni, que reunirá informações sobre pequenos e médios empreendedores e ajudará eles com suas finanças pessoais."},
 ]
+
 
 def ask_gpt(question):
     # Add the user's question to the chat log
@@ -41,10 +39,30 @@ def ask_gpt(question):
     chat_log.append({"role": "assistant", "content": assistant_response})
 
     return assistant_response
-
-# Example usage
-ask_gpt('Computei hoje uma venda de 200 reais e ontem uma de 600 reais')
-
-
-
-ask_gpt('Quanto lucrei entre ontem e hoje?')
+    
+def transcript_audio(media_url: str) -> dict:
+    try:
+        ogg_file_path = f'{config.OUTPUT_DIR}/{uuid.uuid1()}.ogg'
+        data = requests.get(media_url)
+        with open(ogg_file_path, 'wb') as file:
+            file.write(data.content)
+        audio_data, sample_rate = sf.read(ogg_file_path)
+        mp3_file_path = f'{config.OUTPUT_DIR}/{uuid.uuid1()}.mp3'
+        sf.write(mp3_file_path, audio_data, sample_rate)
+        audio_file = open(mp3_file_path, 'rb')
+        os.unlink(ogg_file_path)
+        os.unlink(mp3_file_path)
+        transcript = openai.Audio.transcribe(
+            'whisper-1', audio_file, api_key=config.OPENAI_API_KEY)
+        return {
+            'status': 1,
+            'transcript': transcript['text']
+        }
+    except Exception as e:
+        print('Error at transcript_audio...')
+        print(e)
+        return {
+            'status': 0,
+            'transcript': transcript['text']
+        }
+    
--- src/data_parsing.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd 
-
-df = pd.read_parquet('../data/mcc.parquet',engine='pyarrow')
-
-print(df.head())
